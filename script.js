b9e13e227802d2ef6cfa7ed11ebb9eed0255aa2e
Emil Abdulxalıqov <emilabdulxaliqov@Emils-MacBook-Air.local>
2021-05-27 15:51:43 +0400

    just design

---
+++ script.js
@@ -0,0 +1,8 @@
+function imgSlider(images) {
+    document.querySelector('.starbucks').src=images;
+
+}
+function changeColor(color) {
+    const circle=document.querySelector('.circle');
+    circle.style.background=color;
+}
