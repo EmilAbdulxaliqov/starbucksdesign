e943df7e76279cebac172b6a076f2e58f1fd172a
EmilAbdulxaliqov <83636717+EmilAbdulxaliqov@users.noreply.github.com>
2021-05-28 02:43:29 +0400

    Update script.js
    something setup

--- script.js
+++ script.js
@@ -4,5 +4,9 @@ function imgSlider(images) {
 }
 function changeColor(color) {
     const circle=document.querySelector('.circle');
+    const learnMore=document.querySelector('.learn-more');
+    const span=document.querySelector('.span');
     circle.style.background=color;
+    learnMore.style.background=color;
+    span.style.color=color;
 }
